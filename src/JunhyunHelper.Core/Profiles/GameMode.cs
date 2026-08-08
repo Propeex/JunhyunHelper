34d8639791bb05df236227aad409b1894ed76a29
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-08 12:22:38 +0900

    Centralize game mode data keys

--- src/JunhyunHelper.Core/Profiles/GameMode.cs
+++ src/JunhyunHelper.Core/Profiles/GameMode.cs
@@ -6,3 +6,14 @@ public enum GameMode
     Pve,
     PvpSeason,
 }
+
+public static class GameModeExtensions
+{
+    public static string ToDataKey(this GameMode gameMode) => gameMode switch
+    {
+        GameMode.Regular => "regular",
+        GameMode.Pve => "pve",
+        GameMode.PvpSeason => "pvp-season",
+        _ => throw new ArgumentOutOfRangeException(nameof(gameMode), gameMode, null),
+    };
+}
