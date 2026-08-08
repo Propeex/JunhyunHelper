b25b36559076c9b5de6e5f9dc6ecf3484d6481bd
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-08 20:48:24 +0900

    Share image cache with hideout

--- src/JunhyunHelper.Desktop/MainWindow.Images.cs
+++ src/JunhyunHelper.Desktop/MainWindow.Images.cs
@@ -6,4 +6,7 @@ public partial class MainWindow
 {
     private void ItemsPage_Loaded(object sender, RoutedEventArgs e) =>
         ItemsPage.SetImageCache(_services.Images);
+
+    private void HideoutPage_Loaded(object sender, RoutedEventArgs e) =>
+        HideoutPage.SetImageCache(_services.Images);
 }
