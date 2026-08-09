1e0be57ec960b9439bc983d62328b20d7156e3f7
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-09 10:36:01 +0900

    test: lock conventional ammo caliber labels

---
+++ tests/JunhyunHelper.Tests/Core/AmmoCaliberDisplayTests.cs
@@ -0,0 +1,23 @@
+using JunhyunHelper.Core.Ammo;
+using Xunit;
+
+namespace JunhyunHelper.Tests.Core;
+
+public sealed class AmmoCaliberDisplayTests
+{
+    [Theory]
+    [InlineData("Caliber784x49", ".308 Marlin Express")]
+    [InlineData("Caliber93x64", "9.3×64mm")]
+    [InlineData("Caliber9x18PM", "9×18mm Makarov")]
+    [InlineData("Caliber127x33", ".50 Action Express")]
+    [InlineData("Caliber127x108", "12.7×108mm")]
+    [InlineData("Caliber1143x23ACP", ".45 ACP")]
+    [InlineData("Caliber762x35", ".300 Blackout")]
+    [InlineData("Caliber86x70", ".338 Lapua Magnum")]
+    [InlineData("Caliber366TKM", ".366 TKM")]
+    [InlineData("Caliber12g", "12/70")]
+    public void UsesConventionalTarkovLabels(string rawCaliber, string expected)
+    {
+        Assert.Equal(expected, AmmoCaliberDisplay.GetLabel(rawCaliber));
+    }
+}
