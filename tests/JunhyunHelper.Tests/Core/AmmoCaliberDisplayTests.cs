fb7a72637a87e503da8ccbd3d8505bb72e27a0a5
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-09 10:46:04 +0900

    test: require exact cartridge label typography

--- tests/JunhyunHelper.Tests/Core/AmmoCaliberDisplayTests.cs
+++ tests/JunhyunHelper.Tests/Core/AmmoCaliberDisplayTests.cs
@@ -7,10 +7,10 @@ public sealed class AmmoCaliberDisplayTests
 {
     [Theory]
     [InlineData("Caliber784x49", ".308 Marlin Express")]
-    [InlineData("Caliber93x64", "9.3×64mm")]
-    [InlineData("Caliber9x18PM", "9×18mm Makarov")]
+    [InlineData("Caliber93x64", "9.3x64mm")]
+    [InlineData("Caliber9x18PM", "9x18mm Makarov")]
     [InlineData("Caliber127x33", ".50 Action Express")]
-    [InlineData("Caliber127x108", "12.7×108mm")]
+    [InlineData("Caliber127x108", "12.7x108mm")]
     [InlineData("Caliber1143x23ACP", ".45 ACP")]
     [InlineData("Caliber762x35", ".300 Blackout")]
     [InlineData("Caliber86x70", ".338 Lapua Magnum")]
