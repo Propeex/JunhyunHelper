8ec18d8ad32a95218c699b99537081efbd3d5f93
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-10 10:59:32 +0900

    Restore Map marker DB service alias

--- src/JunhyunHelper.Desktop/Map/MapProductGlobalUsings.cs
+++ src/JunhyunHelper.Desktop/Map/MapProductGlobalUsings.cs
@@ -1,2 +1,3 @@
 global using JunhyunHelper.Application.Quests;
 global using FloorDetectionService = TarkovHelper.Services.FloorDetectionService;
+global using MapMarkerDbService = TarkovHelper.Services.MapMarkerDbService;
