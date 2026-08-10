fea3f743bd8eca1268e4c0daecde7485c2da2389
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-10 11:00:49 +0900

    Fix Raider marker service namespace

--- src/JunhyunHelper.Desktop/Map/LegacyAdditionalMapMarkers.cs
+++ src/JunhyunHelper.Desktop/Map/LegacyAdditionalMapMarkers.cs
@@ -4,6 +4,7 @@ using System.Windows.Media;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using TarkovHelper.Models;
+using TarkovHelper.Services;
 using TarkovHelper.Services.Map;
 
 namespace JunhyunHelper.Desktop.Map;
