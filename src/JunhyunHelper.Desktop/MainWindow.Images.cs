eb92f02930c4313fe6eff08d05fb023304ad1437
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-27 13:42:10 +0900

    Remove Loaded-order wiring side effects

--- src/JunhyunHelper.Desktop/MainWindow.Images.cs
+++ src/JunhyunHelper.Desktop/MainWindow.Images.cs
@@ -1,4 +1,3 @@
-using System.Windows;
 using JunhyunHelper.Desktop.Ammo;
 using JunhyunHelper.Desktop.Hideout;
 using JunhyunHelper.Desktop.Items;
@@ -9,27 +8,6 @@ namespace JunhyunHelper.Desktop;
 public partial class MainWindow
 {
     private bool _contentNavigationAttached;
-
-    private void ItemsPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        ItemsPage.SetImageCache(_services.Images);
-        QuestPage.SetImageCache(_services.Images);
-        QuestPage.RefreshNavigationLinks();
-        AttachContentNavigation();
-    }
-
-    private void HideoutPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        HideoutPage.SetImageCache(_services.Images);
-        AttachContentNavigation();
-    }
-
-    private void AmmoPage_Loaded(object sender, RoutedEventArgs e)
-    {
-        AmmoPage.SetImageCache(_services.Images);
-        AmmoPage.SetFavoriteStore(_services.AmmoFavorites);
-        AttachContentNavigation();
-    }
 
     private void AttachContentNavigation()
     {
