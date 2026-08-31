3928d77db305b6f828749d362f7d920d7f2de2ca
Propeex <60839060+Propeex@users.noreply.github.com>
2026-08-31 13:11:18 +0900

    fix: integrate farming guide with main section navigation

--- src/JunhyunHelper.Desktop/MainWindow.FarmingGuide.cs
+++ src/JunhyunHelper.Desktop/MainWindow.FarmingGuide.cs
@@ -1,12 +1,9 @@
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace JunhyunHelper.Desktop;
 
 public partial class MainWindow
 {
-    private bool _farmingGuideHooksInstalled;
     private bool _farmingGuideConfigured;
 
     private void FarmingGuideTabButton_Click(object sender, RoutedEventArgs e)
@@ -14,79 +11,16 @@ public partial class MainWindow
         if (_activeProfile is null || _activeContent is null)
             return;
 
-        EnsureFarmingGuideHooks();
-        EnsureFarmingGuideConfigured();
-        FarmingGuidePage.SetData(_activeContent, _activeProfile.ProfileId);
-
-        QuestPage.Visibility = Visibility.Collapsed;
-        HideoutPage.Visibility = Visibility.Collapsed;
-        ItemsPage.Visibility = Visibility.Collapsed;
-        AmmoPage.Visibility = Visibility.Collapsed;
-        MapPlaceholder.Visibility = Visibility.Collapsed;
-        ScannerPlaceholder.Visibility = Visibility.Collapsed;
-        EmptyState.Visibility = Visibility.Collapsed;
-        FarmingGuidePage.Visibility = Visibility.Visible;
-
-        SetFarmingGuideHeaderSelected(true);
+        _activeSection = DesktopSection.FarmingGuide;
+        ShowActiveSection();
     }
 
     private void EnsureFarmingGuideConfigured()
     {
         if (_farmingGuideConfigured)
             return;
+
         FarmingGuidePage.Configure(_services.Images, _services.FarmingGuide);
         _farmingGuideConfigured = true;
-    }
-
-    private void EnsureFarmingGuideHooks()
-    {
-        if (_farmingGuideHooksInstalled)
-            return;
-        _farmingGuideHooksInstalled = true;
-
-        QuestTabButton.Click += ExistingSectionButton_Click;
-        HideoutTabButton.Click += ExistingSectionButton_Click;
-        ItemsTabButton.Click += ExistingSectionButton_Click;
-        AmmoTabButton.Click += ExistingSectionButton_Click;
-        MapTabButton.Click += ExistingSectionButton_Click;
-        ScannerTabButton.Click += ExistingSectionButton_Click;
-        ProfileComboBox.SelectionChanged += FarmingGuideProfileComboBox_SelectionChanged;
-    }
-
-    private void ExistingSectionButton_Click(object sender, RoutedEventArgs e)
-    {
-        FarmingGuidePage.Visibility = Visibility.Collapsed;
-        SetFarmingGuideHeaderSelected(false);
-    }
-
-    private async void FarmingGuideProfileComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (FarmingGuidePage.Visibility != Visibility.Visible)
-            return;
-
-        await Dispatcher.InvokeAsync(() => { }, System.Windows.Threading.DispatcherPriority.ContextIdle);
-        if (_activeProfile is null || _activeContent is null)
-        {
-            FarmingGuidePage.Visibility = Visibility.Collapsed;
-            SetFarmingGuideHeaderSelected(false);
-            return;
-        }
-
-        EnsureFarmingGuideConfigured();
-        FarmingGuidePage.SetData(_activeContent, _activeProfile.ProfileId);
-    }
-
-    private void SetFarmingGuideHeaderSelected(bool selected)
-    {
-        if (selected)
-        {
-            FarmingGuideTabButton.Background = (Brush)FindResource("BackgroundHoverBrush");
-            FarmingGuideTabButton.BorderBrush = (Brush)FindResource("AccentBrush");
-        }
-        else
-        {
-            FarmingGuideTabButton.ClearValue(Control.BackgroundProperty);
-            FarmingGuideTabButton.ClearValue(Control.BorderBrushProperty);
-        }
     }
 }
