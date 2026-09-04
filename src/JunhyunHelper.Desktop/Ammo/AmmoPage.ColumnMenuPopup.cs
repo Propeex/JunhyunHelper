4d5fe97690a948b187f0b0ffae8a8b3accbf5834
Propeex <60839060+Propeex@users.noreply.github.com>
2026-09-04 14:04:41 +0900

    refactor: name Ammo column popup behavior directly

---
+++ src/JunhyunHelper.Desktop/Ammo/AmmoPage.ColumnMenuPopup.cs
@@ -0,0 +1,37 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace JunhyunHelper.Desktop.Ammo;
+
+public partial class AmmoPage
+{
+    protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left &&
+            FindAncestor<Button>(e.OriginalSource as DependencyObject) is { } button)
+        {
+            if (ReferenceEquals(button, ColumnMenuButton) && ColumnMenuPopup.IsOpen)
+            {
+                ColumnMenuPopup.IsOpen = false;
+                e.Handled = true;
+                return;
+            }
+        }
+
+        base.OnPreviewMouseDown(e);
+    }
+
+    private static T? FindAncestor<T>(DependencyObject? source)
+        where T : DependencyObject
+    {
+        for (var current = source; current is not null; current = VisualTreeHelper.GetParent(current))
+        {
+            if (current is T match)
+                return match;
+        }
+
+        return null;
+    }
+}
